cc27fec72969e84077f696fb9b4ee861542a1df0
jaynopponep <noppone.p@gmail.com>
2024-07-12 17:49:08 -0400

    adding basic dir layout for pages

---
+++ src/app/pages/my-schedule/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Schedule = () => {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">My Schedule</h1>
+            <p>My Schedule</p>
+        </div>
+    );
+};
+
+export default Schedule;
