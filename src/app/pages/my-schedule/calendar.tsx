bae9e75377f0d8b2e8fc6e48365e2575d2fb5105
William Ng <wng003@citymail.cuny.edu>
2024-07-22 07:07:54 -0400

    calender display db, course creation page created

--- src/app/pages/my-schedule/calendar.tsx
+++ src/app/pages/my-schedule/calendar.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-react';
-import signIn from "../sign-in/page";
 
 export default function Calendar() {
   const [calendar, setCalendar] = useState<DayPilot.Calendar>();
@@ -27,19 +26,25 @@ export default function Calendar() {
     setStartDate(startDate.addDays(7));
   };
 
+  interface EventDataWithContextMenu extends DayPilot.EventData {
+    ContextMenu: DayPilot.Menu;
+  }
+  
   const onBeforeEventRender = (args: DayPilot.CalendarBeforeEventRenderArgs) => {
     args.data.areas = [
       {
-        top: 5,
         right: 5,
+        bottom: 5,
         width: 20,
         height: 20,
         fontColor: "#fff",
         backColor: "#00000033",
         style: "border-radius: 25%; cursor: pointer;",
+        action: "ContextMenu",
+        visibility: "Hover",
       },
     ];
-
+  
     const students = args.data.tags?.students || 0;
     if (students > 0) {
       args.data.areas.push({
@@ -54,6 +59,19 @@ export default function Calendar() {
         style: "border-radius: 50%; border: 2px solid #fff; font-size: 18px; text-align: center;",
       });
     }
+  
+    const ContextMenu = new DayPilot.Menu({
+      items: [
+        {
+          text: 'Details',
+          onClick: (args) => {
+            const e = args.source; DayPilot.Modal.prompt("Description: " + e.data.courseDesc);
+          },
+        },
+      ],
+    });
+  
+    (args.data as EventDataWithContextMenu).ContextMenu = ContextMenu;
   };
 
   const initialConfig: DayPilot.CalendarConfig = {
@@ -87,71 +105,75 @@ export default function Calendar() {
 
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    const getDateForDayName = (startDate: DayPilot.Date, dayName: string) => {
-      console.log("start date is ", startDate);
+    const getDatebydayName = (startDate: DayPilot.Date, dayName: string) => {
+      // console.log("start date is ", startDate); // check the start date of the week
       const dayIndex = daysOfWeek.indexOf(dayName);
       const startDayIndex = startDate.getDayOfWeek();
       const diff = dayIndex - startDayIndex;
       return startDate.addDays(diff).toString();
     };
-    // const user = localStorage.getItem("userID");
+    // const user = localStorage.getItem("userID"); // Get the user ID from local storage 
     // console.log("user is ", user);
 
     const fetchData = async () => {
       try {
-        const apiCall = `/api/select?table=course&columns=course.courseID,course.courseName,course.startTime,course.endTime,GROUP_CONCAT(days.dayName) AS dayNames,professor.fullname,course.courseDesc&inner_join=course_days&on_inner=course.courseID=course_days.courseID&inner_join=days&on_inner=course_days.dayID=days.dayID&inner_join=professor&on_inner=course.profID=professor.profID&inner_join=enrollment&on_inner=course.courseID=enrollment.courseID&inner_join=student&on_inner=enrollment.studentID=student.studentID&inner_join=users&on_inner=student.userID=users.userID&condition=users.userType='student' AND users.userID=1&group_by=course.courseID&order_by=course.startTime`;
+        const apiCall = `/api/select?table=course&columns=course.courseID,course.courseName,course.startTime,course.endTime,GROUP_CONCAT(days.dayName) AS dayNames,professor.fullname,course.courseDesc&inner_join=course_days&on_inner=course.courseID=course_days.courseID&inner_join=days&on_inner=course_days.dayID=days.dayID&inner_join=professor&on_inner=course.profID=professor.profID&inner_join=enrollment&on_inner=course.courseID=enrollment.courseID&inner_join=student&on_inner=enrollment.studentID=student.studentID&inner_join=users&on_inner=student.userID=users.userID&condition=users.userType='student' AND users.userID=1&group_by=course.courseID&order_by=course.startTime`; // user.userID = ${user} fetches the ID for the current user
         const response = await fetch(apiCall, {
           method: "GET",
         });
   
         if (!response.ok) {
-          throw new Error("Could not retrieve events");
+          throw new Error("Could not retrieve Courses");
         }
   
         const data = await response.json();
         console.log("Fetched data:", data); 
         if (data.results && Array.isArray(data.results)) {
-          const fetchEvents: DayPilot.EventData[] = data.results.map((event: any) => {
-            const dayNames = event.dayNames.split(','); // Split dayNames into an array
-            const eventStart = getDateForDayName(startDate, dayNames[0]); // Get the date for the first day name
-            const eventEnd = getDateForDayName(startDate, dayNames[0]); // Assuming the end date is the same as the start date
-            console.log("day names are ", dayNames);
-            console.log("event start is ", eventStart);
-            console.log("event end is ", eventEnd);
-            return {
-                    id: event.courseID,
-                    text: event.courseName,
-                    start: updateTime(eventStart, event.startTime),
-                    end: updateTime(eventEnd, event.endTime),
-                    backColor: event.backColor,
-            }
+          const fetchEvents = data.results.flatMap((event: any) => {
+            const dayNames = event.dayNames.split(",");
+            return dayNames.map((dayName: any) => {
+              const eventStart = getDatebydayName(startDate, dayName);
+              const eventEnd = getDatebydayName(startDate, dayName);
+              return {
+                id: event.courseID,
+                text: event.courseName + "\n ["+ event.startTime +" - " + event.endTime + "] ",
+                start: updateTime(eventStart, event.startTime),
+                end: updateTime(eventEnd, event.endTime),
+                backColor: event.backcolor, 
+                courseDesc: event.courseDesc, 
+                barColor: colors[Math.floor(Math.random() * colors.length)].id,
+                tags: { 
+                  students: 0, 
+                }
+              };
+            });
           });
           // console.log("start time is ", fetchEvents[0].start);
           // console.log("end time is ", fetchEvents[0].end);
+          // console.log("the student courses are ", fetchEvents); // shows the courses the student is enrolled in
           calendar.update({ startDate, events: fetchEvents });
         } else {
-          console.error("Expected an array but received:", data.results);
+          console.error("Expected an array enrolled Courses but received:", data.results);
         }
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching Courses:", error);
       }
     };
-  
     fetchData();
   }, [calendar, startDate]);
   
-
-  const onTimeRangeSelected = async (args: DayPilot.CalendarTimeRangeSelectedArgs) => {
-    const modal = await DayPilot.Modal.prompt("Add new event:", "");
-    calendar?.clearSelection();
-    if (modal.canceled) return;
-    calendar?.events.add({
-      start: args.start,
-      end: args.end,
-      id: DayPilot.guid(),
-      text: modal.result,
-    });
-  };
+  // for creating new events ...
+  // const onTimeRangeSelected = async (args: DayPilot.CalendarTimeRangeSelectedArgs) => {
+  //   const modal = await DayPilot.Modal.prompt("Add new event:", "");
+  //   calendar?.clearSelection();
+  //   if (modal.canceled) return;
+  //   calendar?.events.add({
+  //     start: args.start,
+  //     end: args.end,
+  //     id: DayPilot.guid(),
+  //     text: modal.result,
+  //   });
+  // };
 
   return (
     <div>
@@ -167,7 +189,7 @@ export default function Calendar() {
       <div className="relative justify-center items-center flex-col mx-auto mb-20 w-full max-w-screen-lg">
         <DayPilotCalendar
           {...config}
-          onTimeRangeSelected={onTimeRangeSelected}
+          // onTimeRangeSelected={onTimeRangeSelected} // for creating new events
           onBeforeEventRender={onBeforeEventRender}
           controlRef={setCalendar}
         />
